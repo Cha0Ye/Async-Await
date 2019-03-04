723cbe07556cff2f2790f3c6325d34182202e012
Gabriela <balicas.gb@gmail.com>
2019-03-04 11:29:55 -0800

    complete q1 n3

--- async_await.js
+++ async_await.js
@@ -24,3 +24,23 @@ async function manyLuckyNums(){
     return [a1, a2, a3];
 }
 
+// let response = await manyLuckyNums();
+
+// Question 1 Number 3
+async function manyLuckyNums(){ 
+
+    let p1 = $.get(`${NUMBERS_API_URL}/78`);
+    let p2 = $.get(`${NUMBERS_API_URL}/78`);
+    let p3 = $.get(`${NUMBERS_API_URL}/78`);
+    let p4 = $.get(`${NUMBERS_API_URL}/78`);
+
+    
+    let a1 = await p1;
+    let a2 = await p2;
+    let a3 = await p3;
+    let a4 = await p4;
+
+    $('#facts').html(`<p>${a1}</p> <p>${a2} </p><p>${a3}</p><p>${a4}</p>`)
+}
+
+// await manyLuckyNums();
