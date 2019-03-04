4a39f91c72c111ad638494728cbb8ea34a85a00b
Gabriela <balicas.gb@gmail.com>
2019-03-04 11:17:24 -0800

    finish q1, n2

--- async_await.js
+++ async_await.js
@@ -1,12 +1,26 @@
 const NUMBERS_API_URL = 'http://numbersapi.com/'; 
 
 
+//Question 1 Number 1
 async function singleLuckyNumFact(){ 
-
-    let q1 = await $.get(`${NUMBERS_API_URL}/78`)
+    let q1 = await $.get(`${NUMBERS_API_URL}/78`);
     return q1;
 }
 
-//Number 1
-let resp = await singleLuckyNumFact();
+// let resp = await singleLuckyNumFact();
+
+
+
+// Question 1 Number 2
+async function manyLuckyNums(){ 
+    let p1 = $.get(`${NUMBERS_API_URL}/78`);
+    let p2 = $.get(`${NUMBERS_API_URL}/88`);
+    let p3 = $.get(`${NUMBERS_API_URL}/98`);
+    
+    let a1 = await p1;
+    let a2 = await p2;
+    let a3 = await p3;
+    
+    return [a1, a2, a3];
+}
 
